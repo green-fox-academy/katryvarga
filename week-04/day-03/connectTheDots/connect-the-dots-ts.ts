caea742060ce4a0a516cf4705547f118aee0e0eb
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-01-02 18:58:21 +0100

    connectTheDots

--- week-04/day-03/connectTheDots/connect-the-dots-ts.ts
+++ week-04/day-03/connectTheDots/connect-the-dots-ts.ts
@@ -9,6 +9,33 @@ export {};
 // Create a function that takes 1 parameter:
 // A list of [x, y] points
 // and connects them with green lines.
-// Connect these to get a box: [[10, 10], [290,  10], [290, 290], [10, 290]]
-// Connect these: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
-// [120, 100], [85, 130], [50, 100]]
+// Connect these to get a box: 
+// Connect these: 
+
+let connectingDotsBox: number[][] = [[10, 10], [290,  10], [290, 290], [10, 290]];
+let connectindDots1: number [][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
+
+function connectDots(list) {
+    ctx.strokeStyle = 'green'
+    ctx.beginPath();
+    ctx.moveTo(list[0][0], list[0][1]);
+    for (let i = 0; i < list.length; i++) {
+        ctx.lineTo(list[i][0], list[i][1]);
+    }
+    ctx.closePath();
+    ctx.stroke();
+}
+
+function connectAndFillDots(list) {
+    ctx.fillStyle = 'green'
+    ctx.beginPath();
+    ctx.moveTo(list[0][0], list[0][1]);
+    for (let i = 0; i < list.length; i++) {
+        ctx.lineTo(list[i][0], list[i][1]);
+    }
+    ctx.closePath();
+    ctx.fill();
+}
+
+connectDots(connectingDotsBox);
+connectAndFillDots(connectindDots1);
