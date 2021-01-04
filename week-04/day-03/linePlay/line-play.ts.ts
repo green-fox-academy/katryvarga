ef8caec300a71e4ebf921f431f6094b0c3fbd1a7
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-01-04 21:03:43 +0100

    linePlay

---
+++ week-04/day-03/linePlay/line-play.ts.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export { };
+
+function drawASingleLine(startX: number, startY: number, endX: number, endY: number, color: string) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
+    ctx.stroke();
+}
+
+function linePlay(numberOfLines: number) {
+    for (let i = 0; i < numberOfLines; i++) {
+        drawASingleLine(i * canvas.width / numberOfLines, 0, canvas.width, i * canvas.height / numberOfLines + canvas.height / numberOfLines, 'purple');
+    }
+
+    for (let i = 0; i < numberOfLines; i++) {
+        drawASingleLine(0, i * canvas.height / numberOfLines, i * canvas.width / numberOfLines + canvas.width / numberOfLines, canvas.height, 'green');
+    }
+}
+
+linePlay(20);
