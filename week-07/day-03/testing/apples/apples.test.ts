23e434d64a15a27b93e395d2d9fc18331ce8faad
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-01-23 15:01:08 +0100

    apples test done

---
+++ week-07/day-03/testing/apples/apples.test.ts
@@ -0,0 +1,21 @@
+import * as test from "tape";
+import { Apple } from "./apples";
+
+test(`getApple(myObject) should return apple`, (t) => {
+  const myObject = new Apple();
+
+  const actual = myObject.getApple(`apple`);
+  const expected = `apple`;
+  t.equal(actual, expected);
+  t.end();
+});
+
+// test(`getApple(myObject) should return apple`, (t) => {
+//     const myObject = new Apple();
+  
+//     const actual = myObject.getApple(`pear`);
+//     const expected = `apple`;
+//     t.equal(actual, expected);
+//     t.end();
+//   });
+  
