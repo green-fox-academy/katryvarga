b42223336e22065a644387a2e5d682e9b6d9a05c
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-03-11 21:39:57 +0100

    first server done

--- week-12/day-03/firstServer/app.js
+++ week-12/day-03/firstServer/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
 
 app.get('/', (req, res) => {
-    res.send('sziasztok');
+  res.sendFile(__dirname + '/index.html');
 });
-
-app.get('/ego', (req,res) => {
-    res.send(`${1+1}`);
-});
-
-app.listen(3000);
