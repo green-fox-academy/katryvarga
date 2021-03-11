31a4aec5a8892520e09eeab2b385cf67464a2654
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-03-11 21:59:38 +0100

    done with app.js

--- week-13/day-02/frontEnd/app.js
+++ week-13/day-02/frontEnd/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const path = require('path');
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-})
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
-    res.send('OK!');
-  });
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is up and running on ${port}`);
+});
