1ab2ea8b3a83826d98ad205fc0a8482d9c173175
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-03-13 09:58:07 +0100

    greeter done

--- week-13/day-02/frontEnd/app.js
+++ week-13/day-02/frontEnd/app.js
@@ -17,16 +17,37 @@ app.listen(PORT, () => {
 app.get('/doubling', (req, res) => {
   let input = req.query.input;
   let result;
-  if (input !== undefined){
+  if (input !== undefined) {
     result = {
-      'received': parseInt(input),
-      'result': parseInt(input * 2),
+      received: parseInt(input),
+      result: parseInt(input * 2),
     };
   } else {
     result = {
-      'error': 'Please provide an input!',
+      error: 'Please provide an input!',
     };
   }
   res.send(result);
-})
+});
 
+app.get('/greeter', (req, res) => {
+  let name = req.query.name;
+  let title = req.query.title;
+  let result;
+  if (name !== undefined && title !== undefined) {
+    result = { welcome_message: `Oh, hi there ${name}, my dear ${title}!` };
+  } else if (name === undefined && title === undefined) {
+    result = {
+      error: `Please provide a name and a title!`,
+    };
+  } else if (title === undefined) {
+    result = {
+      error: `Please provide a title!`,
+    };
+  } else {
+    result = {
+      error: `Please provide a name!`,
+    };
+  }
+  res.send(result);
+});
