d5eea7c56d3c691d1ac0b8b16d5dc9b415eb8b6a
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-03-13 14:14:09 +0100

    created app.js added basic backend

---
+++ week-13/day-02/reverseOfTheSith/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+app.post('/sith', (req, res) => {
+
+
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is up and running on ${PORT}`);
+  });
+  
