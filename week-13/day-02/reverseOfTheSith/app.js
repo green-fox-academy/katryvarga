8752d8827d291dae065b509bdfa634abedbf1fab
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-03-13 14:17:57 +0100

    reverse in progress

--- week-13/day-02/reverseOfTheSith/app.js
+++ week-13/day-02/reverseOfTheSith/app.js
@@ -4,13 +4,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
-
-app.post('/sith', (req, res) => {
-
-
-});
+app.post('/sith', (req, res) => {});
 
 app.listen(PORT, () => {
-    console.log(`The server is up and running on ${PORT}`);
-  });
-  
+  console.log(`The server is up and running on ${PORT}`);
+});
