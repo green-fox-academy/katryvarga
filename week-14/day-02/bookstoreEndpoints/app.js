5ec8be36c1451d0a1fe971507f48cb97b60609a0
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-03-30 15:19:39 +0200

    bookstore in progress

--- week-14/day-02/bookstoreEndpoints/app.js
+++ week-14/day-02/bookstoreEndpoints/app.js
@@ -1,21 +1,44 @@
-import express from 'express';
 import mysql from 'mysql';
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+app.use(express.json());
 
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  insecureAuth: `true`
+  database: 'bookinfo',
+  insecureAuth: `true`,
 });
 
 conn.connect((err) => {
   if (err) {
-   console.log('Cannot connect to the database', err);
-   return; 
+    console.log('Cannot connect to the database', err);
+    return;
   }
   console.log('Connection established');
+});
+
+app.get('/titles', (req, res) => {
+  conn.query('SELECT book_name FROM book_mast;', (err, rows) => {
+  if (err) {
+    console.error(`Cannot retrieve data: ${err.toString()}`);
+    res.sendStatus(500);
+    return null;
+  }
+  return res.json(rows);
+  });
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`The server is up and running on ${PORT}`);
 });
