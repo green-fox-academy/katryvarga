1ff021e5b9948c2ac06ea6cbd36309703fe7f4c3
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-03-31 13:15:30 +0200

    star wars done

---
+++ week-13/day-03/XHR/starWars/app.js
@@ -0,0 +1,47 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+let listElement = document.createElement('li');
+const moviesList = document.querySelector('.movies');
+const characters = document.querySelector('.characters');
+
+button.addEventListener('click', function () {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://swapi.dev/api/people/?search=${input.value}`);
+  xhr.onload = () => {
+    let data = JSON.parse(xhr.responseText).results;
+    console.log(data);
+    renderHTML(data);
+  };
+  xhr.send();
+});
+
+listElement.addEventListener('click', function () {
+  const xhr2 = new XMLHttpRequest();
+  xhr2.open('GET', 'https://swapi.dev/api/films/', true);
+  xhr2.onload = () => {
+    const filmData = JSON.parse(xhr2.responseText).results;
+    deleteMovies();
+    getFilms(filmData);
+  };
+
+  xhr2.send();
+});
+
+function renderHTML(data) {
+  for (let i = 0; i < data.length; i++) {
+    listElement.innerHTML = data[i].name;
+    characters.appendChild(listElement);
+  }
+}
+
+function getFilms(filmData) {
+  for (let i = 0; i < filmData.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = `${filmData[i].title}: (${filmData[i].release_date})`;
+    moviesList.appendChild(li);
+  }
+}
+
+function deleteMovies() {
+  moviesList.textContent = '';
+}
