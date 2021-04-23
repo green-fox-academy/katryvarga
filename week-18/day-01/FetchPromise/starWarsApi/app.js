7b643de54321a786bffdedefaa32737401315e55
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-04-23 16:13:57 +0200

    added fetch in star wars api, but still not working

---
+++ week-18/day-01/FetchPromise/starWarsApi/app.js
@@ -0,0 +1,53 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const listElement = document.createElement('li');
+const moviesList = document.querySelector('.movies');
+const characters = document.querySelector('.characters');
+const inputText = input.value;
+const url1 = `https://swapi.dev/api/people/?search=${inputText}`;
+const url2 = 'https://swapi.dev/api/films/';
+
+function renderHTML(data) {
+  for (let i = 0; i < data.length; i++) {
+    listElement.innerHTML = data[i].name;
+    characters.appendChild(listElement);
+  }
+}
+
+function getFilms(filmData) {
+  for (let i = 0; i < filmData.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = `${filmData[i].title}: (${filmData[i].release_date})`;
+    moviesList.appendChild(li);
+  }
+}
+
+function deleteMovies() {
+  moviesList.textContent = '';
+}
+
+button.addEventListener('click', () => {
+  fetch(url1)
+    .then((response) => response.json())
+    .then((result) => {
+      const data = result.results;
+      console.log(data);
+      renderHTML(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+listElement.addEventListener('click', () => {
+  fetch(url2)
+    .then((response) => response.json())
+    .then((result) => {
+      const filmData = result.results;
+      deleteMovies();
+      getFilms(filmData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
