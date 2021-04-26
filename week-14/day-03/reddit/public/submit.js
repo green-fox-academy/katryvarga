e0ea6ee5b9577b125e9059f789c38d8b29cafbae
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-04-26 15:57:34 +0200

    added the alert messages

--- week-14/day-03/reddit/public/submit.js
+++ week-14/day-03/reddit/public/submit.js
@@ -6,25 +6,29 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
   console.log(title.value);
   console.log(url.value);
-
-  fetch('http://localhost:3000/posts', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      title: title.value,
-      url: url.value,
-    }),
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
+  if (!title.value || !url.value) {
+    window.alert('Please fill all the empty fields! ✨');
+  } else {
+    fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: title.value,
+        url: url.value,
+      }),
     })
-    .catch((err) => {
-      console.log(err);
-    });
-  backToMain();
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    window.confirm('Your article was succesfully added to the list. ✔');
+    backToMain();
+  }
 });
 
 function backToMain() {
