8cb37ce4749ed0dbda1f42b5ce3e60424c6005bd
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-04-26 17:02:45 +0200

    added voting and delete function, post method is working fine

--- week-14/day-03/reddit/public/redditFrontend.js
+++ week-14/day-03/reddit/public/redditFrontend.js
@@ -1,63 +1,114 @@
 const posts = document.querySelector('#posts');
 
 window.onload = () => {
+  generatePosts();
+};
+
+function generatePosts() {
   fetch('http://localhost:3000/posts')
     .then((response) => response.json())
     .then((result) => {
       const post = result;
       console.log(post);
-      generatePosts(post);
+      for (let i = 0; i < post.length; i++) {
+        const arrowDiv = document.createElement('div');
+        arrowDiv.className = 'arrowDiv';
+        posts.appendChild(arrowDiv);
+        const buttonUp = document.createElement('button');
+        buttonUp.className = 'button-Up';
+        upVoting(post[i].id, buttonUp);
+        const buttonDown = document.createElement('button');
+        buttonDown.className = 'button-Down';
+        downVoting(post[i].id, buttonDown);
+        const upVote = document.createElement('img');
+        upVote.setAttribute('src', './assets/upvote.png');
+        const downVote = document.createElement('img');
+        downVote.setAttribute('src', './assets/downvote.png');
+        const score = document.createElement('div');
+        score.className = 'score';
+        score.innerText = post[i].score;
+        buttonUp.appendChild(upVote);
+        buttonDown.appendChild(downVote);
+        arrowDiv.appendChild(buttonUp);
+        arrowDiv.appendChild(score);
+        arrowDiv.appendChild(buttonDown);
+        const postDetails = document.createElement('div');
+        postDetails.className = 'post-details';
+        posts.appendChild(postDetails);
+        const postTitle = document.createElement('h2');
+        postTitle.className = 'post-title';
+        postTitle.innerText = post[i].title;
+        const links = document.createElement('div');
+        links.className = 'links';
+        postDetails.appendChild(links);
+        const timeStamp = document.createElement('p');
+        timeStamp.className = 'time-stamp';
+        timeStamp.innerText = post[i].timestamp;
+        postDetails.appendChild(postTitle);
+        links.appendChild(timeStamp);
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete-button';
+        deleteButton.innerHTML = 'Delete';
+        deleteArticle(post[i].id, deleteButton);
+        const modifyButton = document.createElement('button');
+        modifyButton.className = 'modify-button';
+        modifyButton.innerHTML = 'Modify';
+        links.appendChild(deleteButton);
+        links.appendChild(modifyButton);
+      }
     })
     .catch((err) => {
       console.log(err);
     });
-};
-
-function generatePosts(post) {
-  for (let i = 0; i < post.length; i++) {
-    const arrowDiv = document.createElement('div');
-    arrowDiv.className = 'arrowDiv';
-    posts.appendChild(arrowDiv);
-    const buttonUp = document.createElement('button');
-    buttonUp.className = 'button-Up';
-    const buttonDown = document.createElement('button');
-    buttonDown.className = 'button-Down';
-    const upVote = document.createElement('img');
-    upVote.setAttribute('src', './assets/upvote.png');
-    const downVote = document.createElement('img');
-    downVote.setAttribute('src', './assets/downvote.png');
-    const score = document.createElement('div');
-    score.className = 'score';
-    score.innerText = post[i].score;
-    buttonUp.appendChild(upVote);
-    buttonDown.appendChild(downVote);
-    arrowDiv.appendChild(buttonUp);
-    arrowDiv.appendChild(score);
-    arrowDiv.appendChild(buttonDown);
-    const postDetails = document.createElement('div');
-    postDetails.className = 'post-details';
-    posts.appendChild(postDetails);
-    const postTitle = document.createElement('h2');
-    postTitle.className = 'post-title';
-    postTitle.innerText = post[i].title;
-    const links = document.createElement('div');
-    links.className = 'links';
-    postDetails.appendChild(links);
-    const timeStamp = document.createElement('p');
-    timeStamp.className = 'time-stamp';
-    timeStamp.innerText = post[i].timestamp;
-    postDetails.appendChild(postTitle);
-    links.appendChild(timeStamp);
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'delete-button';
-    deleteButton.innerHTML = 'Delete';
-    const modifyButton = document.createElement('button');
-    modifyButton.className = 'modify-button';
-    modifyButton.innerHTML = 'Modify';
-    links.appendChild(deleteButton);
-    links.appendChild(modifyButton);
-  }
 }
 
+function upVoting(id, buttonUp) {
+  buttonUp.addEventListener('click', () => {
+    fetch(`http://localhost:3000/posts/${id}/upvote`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((articles) => {
+        console.log(articles);
+        generatePosts();
+      });
+    location.reload();
+  });
+}
 
-// delete, modify, upVote, downVote function missing 
+function downVoting(id, buttonDown) {
+  buttonDown.addEventListener('click', () => {
+    fetch(`http://localhost:3000/posts/${id}/downvote`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((articles) => {
+        console.log(articles);
+        generatePosts();
+      });
+    location.reload();
+  });
+}
+
+function deleteArticle(id, deleteButton) {
+  deleteButton.addEventListener('click', () => {
+    fetch(`http://localhost:3000/posts/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((articles) => {
+        console.log(articles);
+        generatePosts();
+      });
+    location.reload();
+  });
+}
