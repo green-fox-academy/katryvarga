ceecd3e5a7af34ed7bdcb33b9be978bf037c06dd
Katry-Varga Vanda <varga.vanda88@gmail.com>
2021-05-04 15:41:58 +0200

    get/api/game and get/api/quiestions done

--- week-18/day-03/quizApplication/server.js
+++ week-18/day-03/quizApplication/server.js
@@ -34,6 +34,41 @@ app.get('/questions', (req, res) => {
   res.sendFile(path.join(path.resolve(), 'public/questions.html'));
 });
 
+app.get('/api/game', (req, res) => {
+  conn.query('SELECT count(id) FROM questions', (err, rows) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    } else {
+      const recordNumber = Object.values(rows[0]);
+      const randomNumber = Math.floor(Math.random() * recordNumber + 1);
+      conn.query(
+        'SELECT * FROM questions q LEFT JOIN answers a ON a.question_id = q.id WHERE q.id = ?',
+        [randomNumber],
+        (err, rows) => {
+          if (err) {
+            res.status(500).json(err);
+            return;
+          } else {
+            res.status(200).json(rows);
+          }
+        }
+      );
+    }
+  });
+});
+
+app.get('/api/questions', (req, res) => {
+  conn.query('SELECT * FROM questions', (err, rows) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    } else {
+      res.status(400).json(rows);
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT} ✨`);
 });
